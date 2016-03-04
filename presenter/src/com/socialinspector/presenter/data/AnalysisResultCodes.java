7e359cfe30763f046f3473add10c0991b63ab0ec
Ali Yeşilkanat <aliyesilkanat@gmail.com>
2016-03-04 09:43:24 +0200

    deleted dynamic web project

--- presenter/src/com/socialinspector/presenter/data/AnalysisResultCodes.java
+++
@@ -1,9 +0,0 @@
-package com.socialinspector.presenter.data;
-
-public class AnalysisResultCodes {
-	public static final int NEUTRAL = 0;
-	public static final int POSITIVE = 1;
-	public static final int NEGATIVE = 2;
-	public static final int VERY_POSITIVE = 3;
-	public static final int VERY_NEGATIVE = 4;
-}
